5b122434e3bfbb469481de70b07b4d1a27347d64
RuksadAli786 <asafakahamad231@gmail.com>
2023-06-21 05:27:40 +0300

    created a student array and printed its item

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("i am learning array")
+let student=["ruksad","asafak","sayyed","raju","abida","kaif","affifa","rahul","ashif","ajaj"]
+console.log(student[0])
+console.log(student[1])
+console.log(student[2])
+console.log(student[3])
+console.log(student[4])
+console.log(student[5])
+console.log(student[6])
+console.log(student[7])
+console.log(student[8])
+console.log(student[9])
+student[2] = "kundan"
+console.log(student[2])
+student[10] = "khan"
+console.log(student[10])
+
+console.log("******************************************")
+for(index in student){
+    console.log(student[index])
+}
+
+
